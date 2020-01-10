ea55995a745047e062031a145f099b5d65a60f2c
skyhoffert <skwylarh@yahoo.com>
2020-01-09 21:12:12 -0500

    Working more with the ground object.

--- 8b/index.js
+++ 8b/index.js
@@ -17,17 +17,28 @@ element: document.body,
 engine: engine
 });
 
+var verts = [];
+var nverts = 9;
+var hverts = 20;
+for (let i = 0; i < nverts; i++) {
+    let px = -nverts/2*100 + i*100;
+    let py = Math.random()*hverts - hverts/2;
+    verts.push({x:px,y:py});
+    console.log("px: "+px+", py:"+py);
+}
+verts.push({x:0,y:200});
+
 // create two boxes and a ground
 var boxA = Bodies.rectangle(400, 200, 80, 80);
 var boxB = Bodies.polygon(450, 50, 3, 50, {angle:Math.random()*2*Math.PI});
-var boxC = Bodies.circle(200, 500, 40);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-var pin = Bodies.polygon(400, 540, 3, 40, { isStatic: true, angle:1});
+var boxC = Bodies.circle(200, 400, 20, {density:0.004}, 16);
+var ground = Bodies.fromVertices(400, 540, verts, {isStatic:true}, false);
 
 Matter.Body.applyForce(boxC, boxC.position, {x:0.04,y:-0.14});
 
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, boxC, ground, pin]);
+World.add(engine.world, [boxA, boxB, boxC]);
+World.add(engine.world, ground);
 
 // run the engine
 Engine.run(engine);
